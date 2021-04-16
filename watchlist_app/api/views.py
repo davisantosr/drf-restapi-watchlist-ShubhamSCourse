e77ef2c812190dd211397dd1ffc596a9f19a966b
davisantosr <davi-rocha2@hotmail.com>
2021-04-16 15:24:39 -0300

    function based views using api_view decorator 2

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -9,6 +9,8 @@ def movie_list(request):
   movies = Movie.objects.all()
   serializer = MovieSerializer(movies, many=True)
   return Response(serializer.data)
+
+  
 @api_view()
 def movie_details(request, pk):
   movie = Movie.objects.get(pk=pk)
